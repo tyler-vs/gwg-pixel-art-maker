05698b5628add64ab7111f9b90d5964e38574034
tyler-vs <tylervucf@knights.ucf.edu>
2018-01-26 11:52:34 -0500

    feat: add variables for input elements that generate the pixel grid.

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
 
-function makeGrid() {
+  // Select color input
+  const $colorInput = $('#colorPicker');
 
-// Your code goes here!
+  // Select size input
+  const $sizeForm = $('#sizePicker');
 
-}
+  // When size is submitted by the user, call makeGrid()
+  $sizeForm.on('submit', makeGrid);
+
+  function makeGrid(evt) {
+
+    // prevent form submitting
+    evt.preventDefault();
+    console.log(evt.target);
+
+    const $tableElement = $('#pixelCanvas');
+    const $gridHeightValue = $('#inputHeight').val();
+    const $gridWidthValue = $('#inputWeight').val();
+    console.log('grid height: ' + $gridHeightValue + ' and grid width: ' + $gridWidthValue + '.');
+
+    // create the grid
+    for (var i = 0; i <= $gridHeightValue; i++) {
+      for (var j = 0; j <= $gridHeightValue; j++) {
+
+      }
+    }
+
+
+  };
+
+
+
+});
