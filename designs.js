b194645b1a77f1399c4ce10f05dc9bcd6b9e3be5
tyler-vs <tylervucf@knights.ucf.edu>
2018-01-26 12:51:04 -0500

    feat: complete jquery implementation of the code challenge

--- designs.js
+++ designs.js
@@ -12,25 +12,51 @@ $(document).ready(function() {
 
   function makeGrid(evt) {
 
-    // prevent form submitting
+    // prevent form's default submit
     evt.preventDefault();
-    console.log(evt.target);
 
+    // debug
+    // console.log(evt.target);
+
+    // set up variables
     const $tableElement = $('#pixelCanvas');
     const $gridHeightValue = $('#inputHeight').val();
     const $gridWidthValue = $('#inputWeight').val();
-    console.log('grid height: ' + $gridHeightValue + ' and grid width: ' + $gridWidthValue + '.');
+
+    // debug
+    // console.log('grid height: ' + $gridHeightValue + ' and grid width: ' + $gridWidthValue + '.');
+
+    // clear the current grid
+    $tableElement.empty();
 
     // create the grid
-    for (var i = 0; i <= $gridHeightValue; i++) {
-      for (var j = 0; j <= $gridHeightValue; j++) {
+    for (var i = 1; i <= $gridHeightValue; i++) {
 
-      }
-    }
+      // create a table row
+      let $trEl = $('<tr></tr>');
 
+      // nested for loop for creating the table cells within the
+      // row.
+      for (var j = 1; j <= $gridWidthValue; j++) {
 
-  };
+        // create table data/cells and append to current table row
+        // $trEl.append('<td>' + j.toString() + '</td>');
+        $trEl.append('<td></td>');
 
+      } // end nested for loop
 
+      // appends table row to the table
+      $tableElement.append($trEl);
+
+    } // end for loop
+
+    // add event delegation to table, we want this to occur only
+    // on table data/cells so `td` is passed as an argument
+    $tableElement.on('click', 'td', function() {
+      // refers to td that was clicked, change it's BG color
+      $(this).css('backgroundColor', $colorInput.val());
+    });
+
+  }
 
 });
