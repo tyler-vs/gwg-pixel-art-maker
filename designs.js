7c91339b630fd8369bc4140e65fcff9003342d40
tyler-vs <tylervucf@knights.ucf.edu>
2018-02-21 10:45:38 -0500

    feat: Add plain js version of project challenge

--- designs.js
+++ designs.js
@@ -1,62 +1,85 @@
+/*
+ * design.js
+ *
+ * plain javascript variant of the pixel art maker challenge, no jQuery
+ *
+ */
 
-$(document).ready(function() {
+
+// wait for page content to load, may not be needed on codepen
+window.addEventListener('load', function() {
+  // 'use strict';
 
   // Select color input
-  const $colorInput = $('#colorPicker');
+  const colorInput = document.getElementById('colorPicker');
 
   // Select size input
-  const $sizeForm = $('#sizePicker');
+  const sizeForm = document.getElementById('sizePicker');
 
   // When size is submitted by the user, call makeGrid()
-  $sizeForm.on('submit', makeGrid);
+  sizeForm.addEventListener('submit', makeGrid, false);
+
+  /**
+   * @description Generates an HTML table element
+   * @param {object} evt - The event object
+   */
 
   function makeGrid(evt) {
 
     // prevent form's default submit
     evt.preventDefault();
 
-    // debug
-    // console.log(evt.target);
-
     // set up variables
-    const $tableElement = $('#pixelCanvas');
-    const $gridHeightValue = $('#inputHeight').val();
-    const $gridWidthValue = $('#inputWeight').val();
-
-    // debug
-    // console.log('grid height: ' + $gridHeightValue + ' and grid width: ' + $gridWidthValue + '.');
+    const tableElement = document.getElementById('pixelCanvas');
+    const gridHeightValue = document.getElementById('inputHeight').value;
+    const gridWidthValue = document.getElementById('inputWeight').value;
 
     // clear the current grid
-    $tableElement.empty();
+    tableElement.innerHTML = '';
 
     // create the grid
-    for (var i = 1; i <= $gridHeightValue; i++) {
+    for (let i = 1; i <= gridHeightValue; i++) {
 
       // create a table row
-      let $trEl = $('<tr></tr>');
+      let trEl = document.createElement('tr');
 
       // nested for loop for creating the table cells within the
       // row.
-      for (var j = 1; j <= $gridWidthValue; j++) {
+      for (let j = 1; j <= gridWidthValue; j++) {
 
         // create table data/cells and append to current table row
-        // $trEl.append('<td>' + j.toString() + '</td>');
-        $trEl.append('<td></td>');
+        trEl.append(document.createElement('td'));
 
       } // end nested for loop
 
       // appends table row to the table
-      $tableElement.append($trEl);
+      tableElement.append(trEl);
 
     } // end for loop
 
     // add event delegation to table, we want this to occur only
     // on table data/cells so `td` is passed as an argument
-    $tableElement.on('click', 'td', function() {
+    tableElement.addEventListener('click', function(evt) {
       // refers to td that was clicked, change it's BG color
-      $(this).css('backgroundColor', $colorInput.val());
+
+      // check if node was a TD elem, if not then do nothing
+      if (evt.target.nodeName.toUpperCase() === 'TD') {
+
+        // check if color value is available
+        if (colorInput && colorInput.value !== undefined) {
+
+          // use value provided
+          evt.target.style.background = colorInput.value;
+        } else {
+          // use errornous red color
+          evt.target.style.background = 'red';
+        } // end if...else
+      } else {
+        // do nothing
+        return;
+      } // end if..else
     });
 
-  }
+  } // end makeGrid function
 
-});
+}); // end of window load event handler
